08404ba217919c8003d7b30cc26c09478f5b92c4
Anthony DeLorenzo <a.ldeloren@gmail.com>
2015-08-07 22:02:48 -0400

    adding initial archive view, updating styles

--- includes/templates/monitor-page.php
+++ includes/templates/monitor-page.php
@@ -6,15 +6,13 @@ if ( ! current_user_can( 'manage_options' ) && ( ! defined( 'DOING_AJAX' ) || ! 
   wp_redirect( site_url() ); 
   exit;
 }
-// Include ajax functions
-include_once( __DIR__ . '/../functions/monitor-ajax.php' );
 // Include single monitor functions
 include_once( __DIR__ . '/../functions/custom-single-page.php' );
 
 wp_head() ?>
 </head>
 <body>
-  <div class="row">
+  <div class="container">
     <div class="col-sm-3 col-md-2 sidebar">
     <h2>Monitoring</h2>
     <?php a1dmonitor_build_sidenav( a1dmonitor_monitors() ); ?>
